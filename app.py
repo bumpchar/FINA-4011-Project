62a14acf347670fc5bab5c3d8be8f169a1a99475
bumpchar <bumpchar@gmail.com>
2026-04-28 21:13:37 -0500

    Update app.py

--- app.py
+++ app.py
@@ -5,6 +5,6 @@ st.set_page_config(page_title="FINA 4011 DCF Valuation App", layout="wide")
 st.title("FINA 4011 DCF Valuation App")
 st.write("This app will estimate the intrinsic value of a stock using a DCF model.")
 
-ticker = st.text_input("Enter Stock Ticker", value="AAPL")
+ticker = st.text_input("Enter Stock Ticker", value="TSM")
 
 st.write(f"You selected: {ticker}")
